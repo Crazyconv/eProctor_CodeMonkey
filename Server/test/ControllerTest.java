9db54d1644f6962b2bb2e5478576a88b08904389
Crazyconv <conanconv@163.com>
2014-04-19 09:44:01 +0800

    Junit test finished

--- Server/test/ControllerTest.java
+++ Server/test/ControllerTest.java
@@ -1,21 +1,33 @@
 import controllers.routes;
-import org.junit.Test;
-import play.mvc.Result;
-import views.html.*;
+import com.avaje.ebean.Ebean;
+import com.google.common.collect.ImmutableMap;
+import org.junit.*;
 
-import static org.fest.assertions.Assertions.assertThat;
-import static play.mvc.Http.Status.OK;
+import play.libs.Yaml;
+import play.mvc.*;
+import play.test.*;
+
+import java.util.List;
+
 import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
 
-// public class ControllerTest {
-//     @Test
-//     public void AdminControllerTest() {
-//         Result result = callAction(
-//                 routes.ref.AdminController.assignInvigilator()
-//         );
-//         assertThat(status(result)).isEqualTo(OK);
-//         assertThat(contentType(result)).isEqualTo("text/html");
-//         assertThat(charset(result)).isEqualTo("utf-8");
-//         assertThat(contentAsString(result)).contains("Access");
-//     }
-// }
+public class ControllerTest {
+    
+	@Before
+    public void setUp() {
+        start(fakeApplication(inMemoryDatabase(), fakeGlobal()));
+        Ebean.save((List) Yaml.load("test-data.yml"));
+    }
+	@Test
+    public void AdminControllerTest() {
+        Result result = callAction(
+                controllers.routes.ref.Application.index(),
+                fakeRequest().withSession("cmuser", "0").withSession("domain", "2")
+        );
+        assertThat(status(result)).isEqualTo(OK);
+        assertThat(contentType(result)).isEqualTo("text/html");
+        assertThat(charset(result)).isEqualTo("utf-8");
+        assertThat(contentAsString(result)).contains("Admin");
+    }
+}
